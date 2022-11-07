f65a6d3e3a0155de7e873574b9b690f23912d57a
April090402 <115001032+April090402@users.noreply.github.com>
2022-11-07 18:41:00 +0700

    Add files via upload

---
+++ Tugas2/Bee.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Bee here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bee extends Actor
+{
+    /**
+     * Act - do whatever the Bee wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(4);
+        if(isAtEdge()){
+            getWorld().removeObject(this);
+        }
+    }
+}

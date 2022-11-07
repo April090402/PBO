5eef9849f5bcba550fa50b9f4d80e65ea76bb86f
April090402 <115001032+April090402@users.noreply.github.com>
2022-11-07 18:32:53 +0700

    Add files via upload

---
+++ Tugas1/Fish.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Fish here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fish extends Actor
+{
+    /**
+     * Act - do whatever the Fish wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(4);
+        if(isAtEdge()){
+            setLocation(0,getY());
+        }
+    }
+}
